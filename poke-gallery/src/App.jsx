58812e51aab2584651246d688f3099bcd7da150d
Roger Monterroso <124108851+Roger2808@users.noreply.github.com>
2025-08-16 14:32:40 -0600

    feat
    update App module to display Pokemon cards with image, name, and type

--- poke-gallery/src/App.jsx
+++ poke-gallery/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    async function fetchPokemons() {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=18");
+      const data = await res.json();
+
+      const details = await Promise.all(
+        data.results.map(async (p) => {
+          const res = await fetch(p.url);
+          return await res.json();
+        })
+      );
+
+      setPokemons(details);
+    }
+
+    fetchPokemons();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="pokemon-grid">
+      {pokemons.map((p) => (
+        <div key={p.id} className="pokemon-card">
+          {/* Todo el contenido dentro del recuadro redondeado */}
+          <img
+            src={p.sprites.other["official-artwork"].front_default}
+            alt={p.name}
+          />
+          <h2>{p.name}</h2>
+          <p>Tipo: {p.types.map((t) => t.type.name).join(", ")}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App
