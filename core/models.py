962bc6b9da513839cddc75f9d81d083d47497d74
Roger Monterroso <124108851+Roger2808@users.noreply.github.com>
2025-09-10 23:46:19 -0600

    feat(models.py): create a third model "Chapter" with a FK

--- core/models.py
+++ core/models.py
@@ -11,3 +11,8 @@ class Book(models.Model):
     title = models.CharField()
     published_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+class Chapter(models.Model):
+    title = models.CharField(max_length=200)
+    number = models.IntegerField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
