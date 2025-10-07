a14c93ca5126b77a353508b8df1ecf4a9f1a2b69
Roger Monterroso <124108851+Roger2808@users.noreply.github.com>
2025-10-07 17:48:19 -0600

    fix: general fixes in core and requirements

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Author, Book, Chapter, Page
 
-# Register your models here.
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Chapter)
+admin.site.register(Page)
